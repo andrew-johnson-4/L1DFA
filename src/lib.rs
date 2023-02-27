140dd0d9792e6e55b277002546c60c764e349f4c
Andrew Johnson <andrew@subarctic.org>
2023-02-27 16:01:42 -0700

    multiple impl blocks makes docs ugly

--- src/lib.rs
+++ src/lib.rs
@@ -18,39 +18,35 @@ impl DFA {
     };
     d.states[at as usize]
   }
-}
 
-impl DFA {
-   pub fn intersect(self: &DFA, right: &DFA) -> DFA {
-      let left = self;
-      let mut p = DFA {
-         states: vec![false; left.states.len() * right.states.len()],
-         transitions: std::collections::HashMap::new(),
-      };
-      for pi in 0..p.states.len() {
-         let li = pi / right.states.len();
-         let ri = pi % right.states.len();
-         if left.states[li] && right.states[ri] {
-            p.states[pi] = true;
-         }
+  pub fn intersect(self: &DFA, right: &DFA) -> DFA {
+    let left = self;
+    let mut p = DFA {
+      states: vec![false; left.states.len() * right.states.len()],
+      transitions: std::collections::HashMap::new(),
+    };
+    for pi in 0..p.states.len() {
+      let li = pi / right.states.len();
+      let ri = pi % right.states.len();
+      if left.states[li] && right.states[ri] {
+        p.states[pi] = true;
       }
-      for ((ls,lc),lt) in left.transitions.iter() {
-      for ((rs,rc),rt) in right.transitions.iter() {
-      if lc == rc {
-         let ps = ls * (right.states.len() as u64) + rs;
-         let pt = lt * (right.states.len() as u64) + rt;
-         p.transitions.insert((ps,*lc),pt);
-      }}}
-      p
-   }
-}
+    }
+    for ((ls,lc),lt) in left.transitions.iter() {
+    for ((rs,rc),rt) in right.transitions.iter() {
+    if lc == rc {
+      let ps = ls * (right.states.len() as u64) + rs;
+      let pt = lt * (right.states.len() as u64) + rt;
+      p.transitions.insert((ps,*lc),pt);
+    }}}
+    p
+  }
 
-impl DFA {
-   pub fn complement(self: &DFA) -> DFA {
-      let d = self;
-      DFA {
-         states: d.states.iter().map(|c| !c).collect::<Vec<bool>>(),
-         transitions: self.transitions.clone(),
-      }
-   }
+  pub fn complement(self: &DFA) -> DFA {
+    let d = self;
+    DFA {
+      states: d.states.iter().map(|c| !c).collect::<Vec<bool>>(),
+      transitions: self.transitions.clone(),
+    }
+  }
 }
